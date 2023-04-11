d4bfefee2e4282b015f71c53aa25a82854738170
luvghodasara <luv.g1@ahduni.edu.in>
2023-04-11 19:50:39 +0530

    mcg raw code

---
+++ src/pages/end.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Button } from '@mui/material';
+
+function End () {
+  return (
+    <div className='instr'>
+        <h1>Thank you for participating</h1>
+        
+        <Button variant='outlined' color='green'>Restart</Button>
+    </div>
+  );
+};
+
+export default End;
